bc882dbc344b3415ef13e4c7228f88e6106ac61c
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-10-19 00:10:51 +0200

    - Mise à jour de l'authStore

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -5,9 +5,25 @@ export const useAuthStore = defineStore('auth', {
     id: 'auth',
     state: () => ({
         isLoggedIn: !!sessionStorage.getItem('auth_token'),
-        isAdmin: false  // Nouvelle propriété pour vérifier si l'utilisateur est un admin
+        isAdmin: sessionStorage.getItem('isAdmin') === 'true', // Récupérer isAdmin du sessionStorage
+        userRole: JSON.parse(sessionStorage.getItem('userRole')) || [] // Récupérer le rôle du sessionStorage
     }),
+    getters: {
+    },
     actions: {
+        setAuthorizationHeader() {
+            const token = sessionStorage.getItem('auth_token');
+            const role = this.userRole;
+
+            if (token) {
+                api.defaults.headers.common['Authorization'] = 'Bearer ' + token;
+            }
+
+            if (role.includes('admin')) {
+                api.defaults.headers.common['Role'] = 'admin';
+            }
+        },
+
         async login(credentials) {
             try {
                 // Récupération du cookie CSRF
@@ -18,50 +34,57 @@ export const useAuthStore = defineStore('auth', {
 
                 if (response.data.message === 'Logged in successfully.') {
                     this.isLoggedIn = true;
+
+                    // Stocker le token, les données de l'utilisateur et le rôle dans sessionStorage
                     sessionStorage.setItem('auth_token', response.data.token);
+                    sessionStorage.setItem('userData', JSON.stringify(response.data.user));
+                    sessionStorage.setItem('userRole', JSON.stringify(response.data.roles)); // Stocker le rôle ici
+
+                    // Mettre à jour le rôle dans le state
+                    this.userRole = response.data.roles;
 
                     // Vérifiez si l'utilisateur a le rôle 'admin'
-                    if (response.data.roles && response.data.roles.includes('admin')) {
-                        this.isAdmin = true;  // Mettez à jour la propriété isAdmin
+                    if (this.userRole.includes('admin')) {
+                        this.isAdmin = true;
+                        sessionStorage.setItem('isAdmin', 'true'); // Stocker en tant que chaîne
+                    } else {
+                        this.isAdmin = false;
+                        sessionStorage.setItem('isAdmin', 'false');
                     }
                 }
             } catch (error) {
                 console.error("Error logging in:", error.response.data);
                 this.isLoggedIn = false;
-                this.isAdmin = false;  // Réinitialisez la propriété isAdmin en cas d'erreur
+                this.isAdmin = false;
                 sessionStorage.removeItem('auth_token');
+                sessionStorage.removeItem('userData');
+                sessionStorage.removeItem('isAdmin'); // Supprimer isAdmin
+                sessionStorage.removeItem('userRole'); // Supprimer le rôle
             }
         },
 
         async logout() {
-            // Configurer l'en-tête d'authentification avec le token stocké
-            const token = sessionStorage.getItem('auth_token');
-            if (token) {
-                api.defaults.headers.common['Authorization'] = 'Bearer ' + token;
-            }
+            this.setAuthorizationHeader(); // Utiliser le header d'authentification ici
 
             try {
                 const response = await api.post('/api/logout');
 
-                // Supprimer l'en-tête d'authentification après la requête
                 delete api.defaults.headers.common['Authorization'];
 
                 if (response.data.message === 'Logged out successfully.') {
                     this.isLoggedIn = false;
-                    this.isAdmin = false;  // Réinitialisez la propriété isAdmin lors de la déconnexion
-                    sessionStorage.removeItem('auth_token'); // Suppression du token lors de la déconnexion
-                    // Optionnel : rediriger vers la page d'accueil ou de connexion
-                    // this.$router.push({ name: 'home' });
+                    this.isAdmin = false;
+                    this.userRole = [];
+                    sessionStorage.removeItem('auth_token');
+                    sessionStorage.removeItem('userData');
+                    sessionStorage.removeItem('isAdmin'); // Supprimer isAdmin
+                    sessionStorage.removeItem('userRole'); // Supprimer le rôle
                 } else {
                     console.warn("Unexpected response during logout:", response.data.message);
                 }
             } catch (error) {
                 console.error("Error logging out:", error.response.data);
-                // Optionnel : afficher un message d'erreur à l'utilisateur
-                // this.errorMessage = "An error occurred while logging out. Please try again.";
             }
         }
-
-        // ... vous pouvez ajouter d'autres actions si nécessaire
     }
 });

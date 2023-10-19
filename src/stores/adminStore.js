8f6a55964dc0e1f0797ed235f3193ab725fb7513
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-10-19 21:24:28 +0200

    - Mise à jour du Store et ajoute de commentaire

--- src/stores/adminStore.js
+++ src/stores/adminStore.js
@@ -1,37 +1,43 @@
-import { defineStore } from 'pinia';
+import {defineStore} from 'pinia';
 import api from '@/services/api';
 
 export const useAdminStore = defineStore({
+    // ID unique du store pour l'identifier lors de l'utilisation avec Pinia.
     id: 'admin',
 
+    // Initialisation de l'état du store. Actuellement, il stocke une liste d'utilisateurs.
     state: () => ({
         users: [],
     }),
 
-    getters: {},
-
     actions: {
+        // Cette action est utilisée pour définir le header d'autorisation pour toutes les requêtes API.
+        // Elle récupère le token d'authentification depuis sessionStorage et le fixe dans les headers.
         setAuthorizationHeader() {
             const token = sessionStorage.getItem('auth_token');
-
             if (token) {
                 api.defaults.headers.common['Authorization'] = 'Bearer ' + token;
             }
-
         },
 
+        // Cette action est utilisée pour récupérer la liste de tous les utilisateurs depuis l'API.
         async fetchUsers() {
+            // Avant de faire la requête, on définit le header d'autorisation.
             this.setAuthorizationHeader();
             try {
                 const response = await api.get('/api/users');
-                this.users = response.data;
+                this.users = response.data.data;
             } catch (error) {
+                // En cas d'erreur lors de la requête, un message d'erreur est affiché dans la console.
                 console.error("Error fetching users:", error.response.data);
             }
         },
 
-        // ... autres actions ...
+        archiveUser(userId) {
+            this.setAuthorizationHeader();
+
+            return api.delete(`/api/users/${userId}`).then(response => response.data.message);
+        }
+
     }
 });
-
-export default useAdminStore;

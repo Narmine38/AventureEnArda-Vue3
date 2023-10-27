ed670e6483c477b512c2c56ed671246152207307
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-10-27 08:41:11 +0200

    _ Mise à jour axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8000', // URL de base de votre API Laravel
+    baseURL: 'https://lionfish-app-7i8g6.ondigitalocean.app', // URL de base de votre API Laravel
     withCredentials: true // Important pour envoyer et recevoir des cookies
 });
 

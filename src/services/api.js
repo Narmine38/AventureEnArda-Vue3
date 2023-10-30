afb39402f23e165016002b8a41f72217335ad9f7
Narmine38 <cedric.atzori@le-campus-numerique.fr>
2023-10-31 00:51:33 +0100

    - Mise a jour

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://seashell-app-lb965.ondigitalocean.app', // URL de base de votre API Laravel
+    baseURL: 'http://127.0.0.1:8000', // URL de base de votre API Laravel
     withCredentials: true // Important pour envoyer et recevoir des cookies
 });
 
